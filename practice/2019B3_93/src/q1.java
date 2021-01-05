78838754b5bf84c9a11fc71d4ff9f4364453104b
YarinAVI <yarinavisidris100@gmail.com>
2021-01-06 00:27:20 +0200

    ore practice

--- practice/2019B3_93/src/q1.java
+++ practice/2019B3_93/src/q1.java
@@ -1,17 +1,19 @@
 public class q1 {
     public static void main(String[] args) {
-        int arr[] = {5,4,2,1,3};
+        int arr[] = {20,4,2,1,3};
         isSum(arr,8);
     }
     public static boolean isSum(int []a,int num) {
-        return isSum(a,num);
+        return isSum(a,num,0,"");
     }
     public static boolean isSum(int []a,int num,int index,String subset) {
-        if(index == a.length || num < 0) return false;
         if(num==0) {
             System.out.println(subset.substring(0,subset.length()-1));
             return true;
         }
-        return isSum(a,num-a[index],index+1,subset+a[index]+"+") || isSum(a,num,index+1,subset);
+        if(index >= a.length || num <0) return false;
+
+        return isSum(a,num-a[index],index+2,subset+a[index]+"+")
+                || isSum(a,num,index+1,subset);
     }
 }
